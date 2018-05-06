9f1f0ab814e23a3a638cdca4ec8d1305a4c4b0c7
nunuchi <36165551+nunuchi@users.noreply.github.com>
2018-05-05 21:25:54 -0500

    Image Modal Fix

--- reserve_car.js
+++ reserve_car.js
@@ -4,31 +4,52 @@ function autoInit() {
 	var acceptClick = document.getElementById('acceptClick');
 	var declineClick = document.getElementById('declineClick');
 	var selectClick = document.getElementsByClassName('selectClick');
+	var iconList = document.getElementsByClassName('iconSlide');
+	var	imgList = document.getElementsByClassName('imgSlide');
+	var imageModal = document.getElementById('imageModal');
+
 
 	reserveClick.addEventListener('click', function(event) {
 		document.getElementById('carTableDisplay').style.display = "block";
 		document.getElementById('reservedTableDisplay').style.display = "none";
-	})
+	});
 
 	cancelClick.addEventListener('click', function(event) {
 		document.getElementById('carTableDisplay').style.display = "none";
 		document.getElementById('reservedTableDisplay').style.display = "block";
-	})
+	});
 
 	acceptClick.addEventListener('click', function(event) {
 		document.getElementById('userAgreement').style.display = "none"
-	})
+		alert("You're reservation has been sent!")
+	});
 
 	declineClick.addEventListener('click', function(event) {
 		document.getElementById('userAgreement').style.display = "none"
-	})
+	});
 
 	for (var i = 0; i < selectClick.length; i++) {
     	selectClick[i].addEventListener('click', function(event) {
 			document.getElementById('userAgreement').style.display = "block"
 		});
-	}
-	
+	}//For End
 
+	for (var i = 0; i < iconList.length; i++) {
+    	iconList[i].addEventListener('click', function(event) {  
+    		imageModal.style.display = 'block';
+		});
+	}//For End
 
+	imageModal.addEventListener	('click', function(event) {//Clicking the Modal causes for the collapse of it, closing the image and modal
+		imageModal.style.display = 'none';
+		for(var i=0; i<document.getElementsByClassName('imgSlide').length; i++)
+		{
+			document.getElementsByClassName('imgSlide')[i].style.display = "none";
+		}
+	});
 }
+
+function imageModalOpen(num){//Opens the modal and show the specified image in it
+	imageModal.style.display = 'block';
+	document.getElementsByClassName('imgSlide')[num].style.display = "block";
+}//Open Image Modal
